5ee67828aefb80daf17380e7fe72e3e624f9934d
RubyflameWarrior <104446839+RubyflameWarrior@users.noreply.github.com>
2023-09-16 18:35:12 -0400

    0.1.2 - Fixed Motor Declarations

--- src/global.cpp
+++ src/global.cpp
@@ -1,11 +1,12 @@
 #include "main.h"
-pros:: Controller Controller;
-pros:: Motor BLMotor;
-pros:: Motor BRMotor;
-pros:: Motor FLMotor;
-pros:: Motor FRMotor;
-void ControllerDisplay(){
 
+pros:: Controller Controller(pros::E_CONTROLLER_MASTER);
+pros:: Motor BLMotor(10, pros::v5::Motor_Gears::green);
+pros:: Motor BRMotor(20, pros::v5::Motor_Gears::green);
+pros:: Motor FLMotor(18, pros::v5::Motor_Gears::green);
+pros:: Motor FRMotor(8, pros::v5::Motor_Gears::green);
+
+void ControllerDisplay(){
     if(!isReverse) Controller.print(0, 0, "is Reversed: false" );
     else if (isReverse)  Controller.print(0, 0, "is Reversed: true" );
 }
