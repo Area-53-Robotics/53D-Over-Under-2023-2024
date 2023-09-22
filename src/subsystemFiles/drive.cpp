27aafd4c3d517bb23efda00ce441262628f2e930
RubyflameWarrior <104446839+RubyflameWarrior@users.noreply.github.com>
2023-09-22 18:02:00 -0400

    0.1.4 - Fixed Drive Curve

--- src/subsystemFiles/drive.cpp
+++ src/subsystemFiles/drive.cpp
@@ -9,13 +9,14 @@ void DirectionToggle(){
     ControllerDisplay();
 }
 
-float GetCurveOutput(int input){
-    return (std::exp(-20/12.7) + std::exp((std::abs(input) - 127) / 12.7) * (1 - std::exp(-20 / 12.7)));
+float GetCurveOutput(int input) {
+    return (std::exp(-20/12.7)+std::exp((std::abs(input)-127)/12.7)*(1-std::exp(-20/12.7))) * input;
 }
 
-void SetDriveMotors(){
+void SetDriveMotors() {
     LYAxis = Controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     RYAxis = Controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+    
     if(abs(LYAxis) <= 10) LYAxis = 0;
     if(abs(RYAxis) <= 10) RYAxis = 0;
 
