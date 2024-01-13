dc626cb17f5acaf009f461d1676d88c58ddce4dc
Ishika S <132236581+Ish-08@users.noreply.github.com>
2024-01-12 21:35:15 -0500

    1.9.1 - Skills and Auton changes

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -61,47 +61,47 @@ void autonomous() {
 	// setStartingOdomValues();
 	// pros::delay(500);
 	// Controller.clear();
-	/*
+	
 	switch (autonSelect) {
 		case 1:
 			// Controller.print(0, 0, "Left Quals Auton");
-			AutonDrive(8, 2000);
-			AutonTurn('R', 5, 2000);
-			AutonDrive(1, 2000);
+			drive(8, 2000);
+			turn('R', 5, 2000);
+			drive(1, 2000);
 			AutonIntake(1000, true);
-			AutonDrive(-1, 2000);
-			AutonDrive(1, 2000);
-			AutonDrive(-1, 2000);
+			drive(-1, 2000);
+			drive(1, 2000);
+			drive(-1, 2000);
 			turn('R', 1);
-			AutonDrive(-5, 2000);
+			drive(-5, 2000);
 			// Controller.print(1, 0, "Auton Completed");
 			break;
 		case 2:
 			// Controller.print(0, 0, "Right Quals Auton");
 			// Pushes triball
-			AutonDrive(7.5, 1300);
-			AutonTurn('R', 1.5, 1300);
+			drive(7.5, 1300);
+			turn('R', 1.5, 1300);
 			AutonIntake(1000, true);
-			AutonDrive(1.2, 1300);
-			AutonDrive(-1, 1300);
-			AutonTurn('R',3.2,1600);
-			AutonDrive(3.3, 1300);
+			drive(1.2, 1300);
+			drive(-1, 1300);
+			turn('R',3.2,1600);
+			drive(3.3, 1300);
 			AutonIntake(1000, false);
-			AutonTurn('R',3.2,1300);
-			AutonDrive(3.3, 1300);
+			turn('R',3.2,1300);
+			drive(3.3, 1300);
 			AutonIntake(1000, true);
-			AutonDrive(0.5, 1300);
-			AutonDrive(-1.5, 1300);
+			drive(0.5, 1300);
+			drive(-1.5, 1300);
 			// Controller.print(1, 0, "Auton Completed");
 			break;
 		case 3:
 			// Controller.print(0, 0, "Left Elims Auton");
-			AutonDrive(4.5,2000);
+			drive(4.5,2000);
 			AutonIntake(1000, true);
-			AutonDrive(1,2000);
-			AutonDrive(-1.5,2000);
-			AutonDrive(2,2000);
-			AutonDrive(-1.5,2000);
+			drive(1,2000);
+			drive(-1.5,2000);
+			drive(2,2000);
+			decltype(-1.5,2000);
 			// Controller.print(1, 0, "Auton Completed");
 			break;
 		case 4:
@@ -116,13 +116,12 @@ void autonomous() {
 			// Controller.print(0, 0, "No Auton Selected");
 			break;
 		case 7:
-			// Controller.print(0, 0, "Programming Skills");
-			ArmMotor.move_absolute(45, 100);
-			FlywheelMotor.move(127);
-			// pros::delay(45000);
+			//Controller.print(0, 0, "Programming Skills");
+			KickerMotor.move(127);
+			pros::delay(45000);
 			// FlywheelMotor.brake();
 			// Controller.print(1, 0, "Skills Complete");
 			break;
 	}
-	*/
+	
 }
