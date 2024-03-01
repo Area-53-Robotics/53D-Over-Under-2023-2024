276aa16cdf9e16f9485788fb1d1c5249e0be4d53
RubyflameWarrior <104446839+RubyflameWarrior@users.noreply.github.com>
2024-03-01 01:45:30 -0500

    Annotate the autonomous routines

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -67,16 +67,16 @@ void AutonWings(unsigned short int msec, bool activateWings){
 void autonomous() {
 	// Informs the program that the robot is in the autonomous phase
 	GamePhase = 3;
-	// Sets the auton to the "Left Quals" auton
-	// autonSelect = 1;
 	
 	switch (autonSelect) {
 		case 0:
+			// Odometry Test Auton
 			chassis.setPose(0, 0, 0); // X, Y, Heading (degrees)
 			chassis.moveToPoint(0, 30, 50000);
 			// chassis.turnTo(45, 45, 5000);
 			break;
 		case 1:
+			// Left Qualification Match Auton
 			controller.print(0, 0, "Left Quals Auton");
 			chassis.setPose(-48, -60, 135); // X, Y, Heading (degrees)
 			ToggleVerticalPneumaticWings();
@@ -91,52 +91,77 @@ void autonomous() {
 			controller.print(1, 0, "Auton Completed");
 			break;
 		case 2:
-			// Single triball using  PID (made at Gateway)
+			// Right Qualification Match Auton (single triball using PID)
 			controller.print(0, 0, "Right Quals Auton");
+			// Sets the robot's current position as the starting position for LemLib
 			chassis.setPose(0, 0, 0); // X, Y, Heading (degrees)
 			// ToggleVerticalPneumaticWings();
-			 pros::delay(1000);
+			// Delays the next command by 1 second
+			pros::delay(1000);
 			// chassis.turnTo(0,-30,2000);
 			// ToggleVerticalPneumaticWings();
+			// Drives the robot backwards closer to the goal
 			chassis.moveToPoint(0, -18, 3000, false, 75);
 			// ToggleVerticalPneumaticWings();
 			// chassis.moveToPoint(0,-20,3000,false,75);
+			// Rams the robot (with the backside facing front) into the goal, scoring the triball
 			chassis.moveToPoint(0, -36, 3000, false, 75);
+			// Delays the next command by 1 second
 			pros::delay(1000);
+			// Drives the robot away from the goal
 			chassis.moveToPoint(0, -15, 3000, true, 75);
+			// Delays the next command by 1 second
 			pros::delay(1000);
+			// Rams the robot (with the backside facing front) into the goal again to ensure the triball is scored
 			chassis.moveToPoint(0, -48, 3000, false, 127);
+			// Delays the next command by 1 second
 			pros::delay(1000);
+			// Drives the robot away from the goal so that it is not touching the triball at the end of the auton phase
 			chassis.moveToPoint(0, -15, 3000, true, 75);
 			controller.print(1, 0, "Auton Completed");
 			break;
 		case 3:
 			// Left Side Half AWP
 			controller.print(0, 0, "Left Elims Auton");
+			// Sets the robot's current position relative to the middle point of the field
 			chassis.setPose(-48 - (7.0 / 2), -60, 135); // X, Y, Heading (degrees)
+			// Lowers the vertical wings, activating them
 			ToggleVerticalPneumaticWings();
 			pros::delay(250);
+			// Turns to the left to begin descoring the match load triball
 			chassis.turnTo(0, -60, 2000);
 			pros::delay(250);
+			// Drives forward to finish descoring the match load triball
 			chassis.moveToPoint(-45, -60, 2000);
+			// Lifts the vertical wings, deactivating them
 			ToggleVerticalPneumaticWings();
 			pros::delay(250);
+			// Turns to the elevation bar
 			chassis.turnTo(0, -60 - 2, 2000);
 			pros::delay(250);
+			// Drives towards the elevation bar, stopping in time for the ziptie attached to the intake to touch the elevaiton bar
 			chassis.moveToPoint(0 - 20, -60 - 2, 2000);
 			pros::delay(250);
+			// Outtakes the pre-loaded alliance triball onto the other side of the field
 			AutonIntake(1000, true);
 			controller.print(1, 0, "Auton Completed");
 			break;
 		case 4:
 			// in progress in-depth auton
 			controller.print(0, 0, "Right Elims Auton");
+			// Sets the robot's current position relative to the middle point of the field
 			chassis.setPose(-48 - (7.0 / 2), -60, 135); // X, Y, Heading (degrees)
+			// Lowers the vertical wings, activating them
 			ToggleVerticalPneumaticWings();
+			// Drives forward to descore the match load triball
 			chassis.turnTo(0, -18, 2000);
+			// Lifts the vertical wings, deactivating them
 			ToggleVerticalPneumaticWings();
+			// Drives the robot towards the triball under the elevation bar
 			chassis.moveToPoint(-30, -18, 2000);
+			// Activates the intake
 			AutonIntake(1000,false);
+			// Drives the robot forward to intake the triball
 			chassis.moveToPoint(-30,-40,2000,false);
 			/*
 			pros::delay(500);
@@ -157,34 +182,48 @@ void autonomous() {
 		case 7:
 			// Currently curves the robot to push a triball into the goal
 			controller.print(0, 0, "Programming Skills - New Version");
-
+			// Sets the robot's current position relative to the middle point of the field
 			chassis.setPose(-72 + 26.5, -60, 133); // X, Y, Heading (degrees)
+			// Pushes the initially placed triballs into the close side goal
 			chassis.moveToPose(-60 + 5, -24 + 24, 180, 2500, {.forwards = false, .chasePower = 8});
+			// Delays the next command until the drivetrain is done moving
 			chassis.waitUntilDone();
+			// Re-sets the position coordinates of the robot to account for wheel slippage
 			chassis.setPose(-72 + 26.5, -29.25, 180); // X, Y, Heading (degrees)
+			// The robot drives backwards into loading position
 			chassis.moveToPose(-48, -48 + 3, 70, 2500, {.forwards = false, .chasePower = 8 + 6});
+			// Delays the next command until the drivetrain is done moving
 			chassis.waitUntilDone();
+			// Activates the vertical wings so that they are touching the match load bar
 			ToggleVerticalPneumaticWings();
+			// 0.5 second delay to allow the wings to fall
 			pros::delay(500);
-			
+			// Activates the kicker
 			//KickerMotor.move(127);
+			// Allows the kicker to shoot for 25 seconds
 			//pros::delay(25000);
+			// Brakes the kicker
 			//KickerMotor.brake();
+			// 0.5 second delay to allow the kicker to stop moving
 			pros::delay(500);
+			// Deactivates the vertical wings
 			ToggleVerticalPneumaticWings();
+			// 0.5 second delay to allow the wings to rise
 			pros::delay(500);
+			// Turns the robot towards the bottom of the field
 			chassis.turnTo( -36,  -60,  2500);
+			// Drives the robot towards the bottom of the field
 			chassis.moveToPoint(-36, -60, 2500);
+			// Turns the robot towards the elevation bar
 			chassis.turnTo(0,-60,2500);
+			// Drives towards the elevation bar
 			chassis.moveToPoint(24, -60, 2500);
 			//chassis.moveToPose(36,-36,0,2500);
+			// Delays the next command until the drivetrain is done moving
 			//chassis.waitUntilDone();
 			//ToggleVerticalPneumaticWings();
 			//chassis.moveToPose(60, -12, 90, 2500);
 			
-			// chassis.moveToPose(-48 - 5, -48 + 8, 70, 2500, {.forwards = false, .chasePower = 8});
-			// TODO: Ishika, test line 192 (directly above) to see if it gets the bot closer to the match load position than line 191
-
 			controller.print(1, 0, "Skills Complete");
 			break;
 		case 8:
